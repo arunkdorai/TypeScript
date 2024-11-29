e03d99a861016d7c87cb896bde1711cf5ac68a34
arunkdorai <arunkumar.k0786@gmail.com>
2024-11-29 16:48:49 +0530

    Till Objects

--- 02Basics/02objects.ts
+++ 02Basics/02objects.ts
@@ -6,10 +6,24 @@ const User = {
 
 function createUser({name: string, isPaid: boolean}) {}
 
-createUser({name: "arun", isPaid: false})
+let newUser = {name: "arun", isPaid: false, email: "arun@gmail.com"}
+
+createUser(newUser)
 
 function createCourse():{name: string, price: number}{
     return {name: "reactjs", price: 399}
 }
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
+}
+
+function createUser1(user: User): User{
+    return {name: "", email: "", isActive: true}
+}
+
+createUser1({name: "", email: "", isActive: true}) 
 
 export {}
