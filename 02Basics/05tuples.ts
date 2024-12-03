0fbc585f2450ca503cb635320b83e8184cd08287
arunkdorai <arunkumar.k0786@gmail.com>
2024-12-03 11:39:06 +0530

    Tuples

---
+++ 02Basics/05tuples.ts
@@ -0,0 +1,21 @@
+// const user4: (string | number)[] = [1, "ar"]
+let user4: [string, number, boolean]
+
+user4 = ["ar", 131, true]
+
+let rgb: [number, number, number] = [244, 123, 211]
+
+type User5 = [number, string]
+
+const newUser: User5 = [112, "exm@google.com"]
+
+newUser[1] = "ar@g.com"
+
+// newUser.push(true)
+
+
+
+
+
+
+export {}
