82fe08487ace542a55d018ceb3b5c15092f8d9f5
arunkdorai <arunkumar.k0786@gmail.com>
2024-12-03 15:29:22 +0530

    Till enums

---
+++ 02Basics/06enums.js
@@ -0,0 +1,1 @@
+var arSeat = "aisle" /* SeatChoice.AISLE */;
---
+++ 02Basics/06enums.ts
@@ -0,0 +1,8 @@
+const enum SeatChoice {
+    AISLE = 'aisle',
+    MIDDLE = 3,
+    WINDOW,
+    SUITE
+}
+
+const arSeat = SeatChoice.AISLE
